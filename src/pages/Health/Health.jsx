7057b7ef1f9a1cd460bed18b86c41f2bd4af304d
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-07-23 21:16:51 +0300

    start work with healthPage

--- src/pages/Health/Health.jsx
+++ src/pages/Health/Health.jsx
@@ -1,10 +1,40 @@
 import { ButtonHome } from 'components/ButtonHome/ButtonHome';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+
 const HealthPage = () => {
+  const [t] = useTranslation();
+
   return (
-    <Link to={'/'}>
-      <ButtonHome />
-    </Link>
+    <>
+      <Link to={'/'}>
+        <ButtonHome />
+      </Link>
+      <div>
+        <img
+          src={require('../../images/healthy/herb1 (1).jpg')}
+          alt=""
+          width="340"
+        />
+        <img
+          src={require('../../images/healthy/herb2 (1).jpg')}
+          alt=""
+          width="340"
+        />
+        <img
+          src={require('../../images/healthy/herb3 (1).jpg')}
+          alt=""
+          width="340"
+        />
+      </div>
+      <div>
+        <h2>{t(`herbalPage.word`)}</h2>
+        <span>{t(`herbalPage.title`)}</span>
+        <p>{t(`herbalPage.text.bread`)}</p>
+        <p>{t(`herbalPage.text.nature`)}</p>
+        <p>{t(`herbalPage.text.herbalTea`)}</p>
+      </div>
+    </>
   );
 };
 
