688ef0fdeef22751249f11045bf81ecf22b4a64f
NataliShkarupeta <113308128+NataliShkarupeta@users.noreply.github.com>
2023-08-14 19:35:30 +0000

    Deploying to gh-pages from @ NataliShkarupeta/Project-DINS@3a428f0c0b8cebc47f65811eca55ea5bf5d76ceb 🚀

--- static/js/728.fbfbe392.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkDins_project=self.webpackChunkDins_project||[]).push([[728],{3728:function(t,n,e){e.d(n,{cyF:function(){return a}});var r=e(9983);function a(t){return(0,r.w_)({tag:"svg",attr:{version:"1",viewBox:"0 0 48 48",enableBackground:"new 0 0 48 48"},child:[{tag:"polygon",attr:{fill:"#3F51B5",points:"29,44 17.3,30 40.7,30"}},{tag:"path",attr:{fill:"#3F51B5",d:"M21,6H8v8h13c2.2,0,4,1.8,4,4v17h8V18C33,11.4,27.6,6,21,6z"}}]})(t)}}}]);
-//# sourceMappingURL=728.fbfbe392.chunk.js.map
